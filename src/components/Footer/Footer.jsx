e4111e30c3018f6506fa7225bca5344e67e6a273
athena272 <guilhermera272@gmail.com>
2024-03-30 20:59:05 -0300

    Create Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.scss'
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>Desenvolvido por Guilherme R. Alves</h2>
+        </footer>
+    )
+}
