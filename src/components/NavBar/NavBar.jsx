625666041ca021fa4132e9ec83448baede77684e
athena272 <guilhermera272@gmail.com>
2024-03-30 20:44:32 -0300

    Create NavBar.jsx

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types"
+import { Link } from "react-router-dom"
+import styles from 'NavBar.module.scss'
+
+NavBar.propTypes = {
+    url: PropTypes.string,
+    children: PropTypes.node,
+}
+
+export default function NavBar({ url, children }) {
+    return (
+        <Link to={url} className={styles.link}>
+            {children}
+        </Link>
+    )
+}
