e2e91a0f748156657c090d396c1df98b142c395a
athena272 <guilhermera272@gmail.com>
2024-03-30 20:59:03 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import AppRoutes from "Routes/AppRoutes";
+import AppRoutes from "./Routes/AppRoutes";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
-    <AppRoutes />
+    <>
+      <AppRoutes />
+      <Footer />
+    </>
   )
 }
