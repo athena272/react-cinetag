b56ef0e66785150206bf563d47f3ec71e725951f
athena272 <guilhermera272@gmail.com>
2024-03-30 20:33:18 -0300

    Create AppRoutes.jsx

---
+++ src/Routes/AppRoutes.jsx
@@ -0,0 +1,12 @@
+import Home from "pages/Home/Home"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export default function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
