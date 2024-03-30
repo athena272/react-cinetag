1d8f08bec0cb31ab52dcf0bb4167aa208b1f2b21
athena272 <guilhermera272@gmail.com>
2024-03-30 20:58:34 -0300

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
-import styles from 'NavBar.module.scss'
+import styles from './NavBar.module.scss'
 
 NavBar.propTypes = {
     url: PropTypes.string,
