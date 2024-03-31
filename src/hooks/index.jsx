2a70dd0c416d085d864b84a990a02c2e20bf0fa4
athena272 <guilhermera272@gmail.com>
2024-03-31 02:40:34 -0300

    Create index.jsx

---
+++ src/hooks/index.jsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { FavoritesContext } from "../contexts/Favorites";
+
+export function useFavoriteContext() {
+    const { favorite, setFavorite } = useContext(FavoritesContext);
+
+    function addFavorite(newFavorite) {
+        const isFavoriteRepeated = favorite.some(item => item.id === newFavorite.id);
+
+        let newList = [...favorite];
+
+        if (!isFavoriteRepeated) {
+            newList.push(newFavorite);
+            return setFavorite(newList);
+        }
+
+        newList.splice(newList.indexOf(newFavorite), 1);
+        return setFavorite(newList);
+    }
+
+    return {
+        favorite,
+        addFavorite
+    };
+}
