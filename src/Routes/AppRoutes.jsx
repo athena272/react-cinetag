f015bc2572b3653ffdf656b97b394eb321d05d1f
athena272 <guilhermera272@gmail.com>
2024-03-31 02:23:06 -0300

    Update AppRoutes.jsx

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,12 +1,21 @@
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Container from "../components/Container/Container";
 import Home from "../pages/Home/Home"
+import Favorites from "../pages/Favorites/Favorites";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-            </Routes>
+            <Header />
+            <Container>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/favorites" element={<Favorites />}></Route>
+                </Routes>
+            </Container>
+            <Footer />
         </BrowserRouter>
     )
 }
