525a0e47f367ceab0d67f57b516593377f07e509
athena272 <guilhermera272@gmail.com>
2024-03-31 01:50:13 -0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
 // import styles from 'Home.module.scss'
+import Banner from '../../components/Banner/Banner'
 import Header from '../../components/Header/Header'
+import Title from '../../components/Title/Title'
 
 export default function Home() {
 
     return (
-        <Header />
+        <>
+            <Header />
+            <Banner image={"home"} />
+            <Title>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Title>
+        </>
     )
 }
