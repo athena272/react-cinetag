c741b26a4401a69e3e009b97cf06051afdeae710
athena272 <guilhermera272@gmail.com>
2024-03-31 02:23:11 -0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,30 @@
 // import styles from 'Home.module.scss'
 import Banner from '../../components/Banner/Banner'
-import Header from '../../components/Header/Header'
+import Card from '../../components/Card/Card'
 import Title from '../../components/Title/Title'
+import videos from '../../db/db.json'
+import styles from './Home.module.scss'
 
 export default function Home() {
 
     return (
         <>
-            <Header />
             <Banner image={"home"} />
             <Title>
                 <h1>Um lugar para guardar seus vídeos e filmes!</h1>
             </Title>
+            {/* <Card title='Gato bonifácio' cover='https://thecatapi.com/api/images/get?format-src&type=png' /> */}
+            <section className={styles.container}>
+                {
+                    videos.map((video, index) => (
+                        <Card
+                            key={index}
+                            cover={video.cover}
+                            title={video.title}
+                        />
+                    ))
+                }
+            </section>
         </>
     )
 }
