10034358f33d2be02d1e8ce0b5c51c735ae1c776
athena272 <guilhermera272@gmail.com>
2024-03-31 04:52:32 -0300

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,29 @@
 import PropTypes from "prop-types"
 import styles from './Card.module.scss'
 import favIcon from '/images/favorite.png'
+import notFavIcon from '/images/favorite_outline.png'
+import { useFavoriteContext } from "../../hooks"
 
 Card.propTypes = {
+    id: PropTypes.number,
     title: PropTypes.string,
     cover: PropTypes.string,
 }
 
-export default function Card({ title, cover }) {
+export default function Card({ id, title, cover }) {
+    const { favorite, addFavorite } = useFavoriteContext()
+    const isFavorite = favorite.some((fav) => fav.id === id);
+    const iconToUse = isFavorite ? favIcon : notFavIcon;
+
     return (
         <div className={styles.container}>
             <img src={cover} alt={title} className={styles.cover} />
             <h2>{title}</h2>
-            <img src={favIcon}
+            <img src={iconToUse}
                 alt="Favoritar filme"
-                className={styles.favorite} />
+                className={styles.favorite}
+                onClick={() => addFavorite({ id, title, favorite })}
+            />
         </div>
     )
 }
