91d1f3a5352e21745de0b77d7f65d4f5326ed0fc
athena272 <guilhermera272@gmail.com>
2024-03-31 05:18:19 -0300

    Update Favorites.jsx

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,14 +1,31 @@
 import styles from './Favorites.module.scss'
 import Banner from '../../components/Banner/Banner'
 import Title from '../../components/Title/Title'
+import { useFavoriteContext } from '../../hooks'
+import Card from '../../components/Card/Card'
 
 export default function Favorites() {
+    const { favorite } = useFavoriteContext()
     return (
         <>
             <Banner image={"favoritos"} />
             <Title>
                 <h1>Meus Favoritos</h1>
             </Title>
+            <section className={styles.container}>
+                {
+                    favorite.map((fav, index) => {
+                        return (
+                            <Card
+                                key={index}
+                                id={fav.id}
+                                cover={fav.cover}
+                                title={fav.title}
+                            />
+                        )
+                    })
+                }
+            </section>
         </>
     )
 }
