7797f49685400647b2208beb178384f5f3eaa9d1
athena272 <guilhermera272@gmail.com>
2024-03-31 04:52:29 -0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -19,6 +19,7 @@ export default function Home() {
                     videos.map((video, index) => (
                         <Card
                             key={index}
+                            id={video.id}
                             cover={video.cover}
                             title={video.title}
                         />
