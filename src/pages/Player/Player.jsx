30b53da644a546ec56c133ae69b6270e57aebee5
athena272 <guilhermera272@gmail.com>
2024-03-31 05:37:04 -0300

    Update Player.jsx

---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,31 @@
+import Banner from '../../components/Banner/Banner'
+import Title from '../../components/Title/Title'
+import styles from './Player.module.scss'
+import videos from '../../db/db.json'
+import { useParams } from 'react-router-dom'
+
+export default function Player() {
+    const parameters = useParams()
+    const video = videos.find(video => video.id === Number(parameters.id)) || videos[0]
+    console.log("🚀 ~ Player ~ video:", video)
+
+    return (
+        <>
+            <Banner image={"player"} />
+            <Title>
+                <h1>Player</h1>
+            </Title>
+            <section className={styles.container}>
+                <iframe
+                    width="560"
+                    height="315"
+                    src={video.link}
+                    title={video.title}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
+                >
+                </iframe>
+            </section>
+        </>
+    )
+}
