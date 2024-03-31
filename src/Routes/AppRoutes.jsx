38dc54bc3aad213c05c28551b402632dc110abcc
athena272 <guilhermera272@gmail.com>
2024-03-31 11:45:08 -0300

    Update AppRoutes.jsx

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,26 +1,21 @@
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
-import Container from "../components/Container/Container";
+import ComumPage from "../pages/ComumPage/ComumPage";
 import Home from "../pages/Home/Home"
 import Favorites from "../pages/Favorites/Favorites";
-import FavoritesProvider from "../contexts/Favorites";
 import Player from "../pages/Player/Player";
+import NotFound from "../pages/NotFound/NotFound";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
-            <Header />
-            <Container>
-                <FavoritesProvider >
-                    <Routes>
-                        <Route path="/" element={<Home />}></Route>
-                        <Route path="/favorites" element={<Favorites />}></Route>
-                        <Route path="/:id" element={<Player />}></Route>
-                    </Routes>
-                </FavoritesProvider>
-            </Container>
-            <Footer />
+            <Routes>
+                <Route path="/" element={<ComumPage />}>
+                    <Route index element={<Home />}></Route>
+                    <Route path="favorites" element={<Favorites />}></Route>
+                    <Route path=":id" element={<Player />}></Route>
+                    <Route path="*" element={<NotFound />}></Route>
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
